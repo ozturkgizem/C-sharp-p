7fa70cfae6cdf7d0f369bb194d074db4f3adde3b
ozturkgizem <44377977+ozturkgizem@users.noreply.github.com>
2020-01-20 12:54:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Kahin OYUNU";
+
+            //repeat etmesi için kesin şart koyuldu.
+            while(0==0)
+            {
+                Console.Clear();
+                //1-99 arasında rastgele sayı tuttuk.
+                Random rastgele = new Random();
+                int sayi = rastgele.Next(1, 99);
+                
+                //döngü
+                int i = 0;
+                while ( i < 5 )
+                {
+                    //tahmin isteme
+                    Console.Write("1-99 arasında bir sayı tahmin ediniz:");
+                    int tahmin = int.Parse(Console.ReadLine());
+
+                    if (tahmin > 1 && tahmin < 99)
+                    {
+                        if (sayi < tahmin)
+                        {
+                            if (i == 4)
+                            {
+                               Console.Write("Doğru cevap:{0}. Üzülmeyin, groot.\n", sayi);
+                            }
+                            else
+                                Console.Write("Aşağı inin.");
+
+                        }
+                        else if (sayi > tahmin)
+                        {
+                            if (i == 4)
+                            {
+                                Console.Write("Doğru cevap:{0}. Üzülmeyin, groot.\n", sayi);
+                            }
+                            else
+                                Console.Write("Yukarı çıkın.");
+                        }
+                        else if (sayi == tahmin)
+                        {
+                            Console.WriteLine("Doğru cevabı buldunuz, tebrikler.");
+                            break;
+                        }
+
+                        i++;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Sayınız verilen aralıkta değil.Lütfen kurallara uyun.");
+                    }
+                }
+
+                Console.WriteLine("Lütfen tekrar oynamak için Enter'a basın.");
+                Console.ReadLine();
+ 
+            } 
+
+
+        }
+    }
+}
